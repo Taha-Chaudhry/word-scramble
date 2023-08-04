9b80bd815c35da2263d82c307a239abbf1ee0c7f
Taha Chaudhry <taha.chaudhry@icloud.com>
2023-08-04 11:36:52 +0300

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Taha Chaudhry on 04/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
